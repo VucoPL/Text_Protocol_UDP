6bfaa37f51ae875fc2416a4fe103a84dd76eccc3
VucoPL <44368870+VucoPL@users.noreply.github.com>
2019-03-23 20:36:43 +0100

    Add files via upload

---
+++ src/klient/wiadomosc.java
@@ -0,0 +1,24 @@
+//@author Michal Najborowski & Maciej Anglart
+
+package klient;
+
+public class wiadomosc {
+
+    //POLE OPERACJI
+    String POWITANIE = "powitanie"; // PRZESYLANY JEST PAKIET POWITALNY
+    String LICZBA = "liczba"; // PRZESYLANA JEST LICZBA/start
+    String WYNIK = "wynik"; // PRZESYLANY JEST WYNIK
+
+    //POLE ODPOWIEDZI
+    String WYGR = "wygrana";
+    String PRZEGR = "przegrana";
+    String BLAD = "blad";
+    String OK = "ok";
+    String ERROR = "error";
+
+    //POLE ID
+    String FULL = "full";
+
+    //POLE NUM
+    String NUM = "num";
+}
